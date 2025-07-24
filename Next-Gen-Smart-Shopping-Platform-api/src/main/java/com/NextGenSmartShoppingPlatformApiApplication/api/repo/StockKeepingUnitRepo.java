819bf72447c41b3face4e6f5612ecbee6e1c3ab3
[200~JkjupiterKing <jupiterkingtechnologies@gmail.com>
2025-07-24 12:32:39 +0530

    frontend: Updated Products, login,sidenav modules Backend : Updated product model, repo and controller.

--- Next-Gen-Smart-Shopping-Platform-api/src/main/java/com/NextGenSmartShoppingPlatformApiApplication/api/repo/StockKeepingUnitRepo.java
+++
@@ -1,8 +0,0 @@
-package com.NextGenSmartShoppingPlatformApiApplication.api.repo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.NextGenSmartShoppingPlatformApiApplication.api.model.StockKeepingUnit;
-@Repository
-public interface StockKeepingUnitRepo extends JpaRepository<StockKeepingUnit, Long> {
-
-}
